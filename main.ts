70f22364bbce1be29ca9b9e7bccaf55f40e7e79c
NPEX42 <NPEX42@gmail.com>
2022-09-19 21:55:04 +0100

    Added 'POST' & 'DELETE' Http Handlers

--- main.ts
+++ main.ts
@@ -1,9 +1,21 @@
 import paths from "./paths.json" assert { type: "json" };
-import { serveDir, serveFile, serve } from "./deps.ts";
+import secret from "./env.json" assert { type: "json" };
+import { serveDir, serveFile, serve, copy } from "./deps.ts";
+
 const port = paths.port;
 
-const handler = (req: Request) => {
+const handler = async (req: Request) => {
+
   const pathname = new URL(req.url).pathname;
+
+  if (req.method == "DELETE") {
+      return await delete_file(req);
+  }
+
+  if (req.method == "POST") {
+    return await post_file(req);
+}
+
   if (pathname === "/") {
 	return serveFile(req, paths.landing);
   }
@@ -19,5 +31,44 @@ const handler = (req: Request) => {
 };
 
 
+
+
 serve(handler, { port });
 
+async function delete_file(req: Request) : Promise<Response> {
+    const body = await req.json();
+    try {
+      if (body.secret_key !== secret.secret) {
+
+        return Promise.reject("Invalid Secret");
+      }
+      Deno.removeSync(paths.static+"/"+body.filepath);
+    } catch (err) {
+
+      console.error("Exception: ", err);
+
+      return Promise.reject(err);
+    }
+
+    return Promise.resolve(new Response());
+}
+
+
+async function post_file(req: Request) : Promise<Response> {
+  const body = await req.json();
+  const contents = body.contents;
+  try {
+    if (body.secret_key !== secret.secret) {
+
+      return Promise.reject("Invalid Secret");
+    }
+    const encoder = new TextEncoder();
+    Deno.writeFileSync(paths.static+"/"+body.filepath, encoder.encode(contents));
+  } catch (err) {
+
+    console.error("Exception: ", err);
+
+    return Promise.reject(Response.error());
+  }
+  return Promise.resolve(Response.json({ok: true}, {status: 200, statusText: "Created File '"+body.filepath+"'"}));
+}

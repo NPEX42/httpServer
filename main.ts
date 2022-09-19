0034186757fa8b2f441ba71d1ef192daa9ea6034
george <npex42@gmail.com>
2022-09-19 13:12:43 +0100

    Added 'port' option to paths.json

--- main.ts
+++ main.ts
@@ -1,7 +1,10 @@
 import { serve } from "https://deno.land/std@0.156.0/http/server.ts";
 import { serveDir, serveFile } from "https://deno.land/std@0.156.0/http/file_server.ts";
 import paths from "./paths.json" assert { type: "json" };
-serve((req) => {
+
+const port = paths.port;
+
+const handler = (req) => {
   const pathname = new URL(req.url).pathname;
   if (pathname === "/") {
 	return serveFile(req, paths.landing);
@@ -13,5 +16,8 @@ serve((req) => {
   }
   // Do dynamic responses
   return new Response();
-});
+};
+
+
+serve(handler, { port });
 
